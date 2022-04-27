4877bd4b419fcf317967ee787370bc9551873067
egorova-sofya <egorovaSjob@yandex.ru>
2022-04-27 17:32:24 +0300

    started nex-js application

---
+++ components/Title/Title.js
@@ -0,0 +1,6 @@
+const Title = ({ tag, text }) => {
+  const Tag = tag || "h1";
+  return <Tag>{text}</Tag>;
+};
+
+export default Title;
--- pages/posts.js
+++ pages/posts.js
@@ -2,6 +2,12 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return <h1>hi</h1>;
-}
+const Posts = () => {
+  return (
+    <>
+      <h1>Posts list</h1>
+    </>
+  );
+};
+
+export default Posts;
